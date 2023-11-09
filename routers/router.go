e8dfad5825e94f499f8687afc0deadfa0761fd94
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-11-09 11:44:59 +0530

    routing added

---
+++ routers/router.go
@@ -0,0 +1,18 @@
+package router
+
+import (
+	"github.com/gorilla/mux"
+	"hey/controllers"
+)
+
+func Router() *mux.Router {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/movies", controllers.GetMyAllMovies).Methods("GET")
+	router.HandleFunc("/api/movie", controllers.CreateMovie).Methods("POST")
+	router.HandleFunc("/api/movie/{id}", controllers.MarkAsWatched).Methods("PUT")
+	router.HandleFunc("/api/movie/{id}", controllers.DeleteAMovie).Methods("DELETE")
+	router.HandleFunc("/api/deleteallmovie", controllers.DeleteAllMovies).Methods("DELETE")
+
+	return router
+}
